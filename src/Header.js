7cc37fbacd50015a129fccb8f97bf87ba600fcba
ShivaSenaReddy <shivasenajagapathi7@gmail.com>
2022-07-25 20:23:37 +0530

    changes

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,7 @@
 export default function Header(props) {
     return (
         <div>
-            <h1>RoboFriends</h1>
+            <h1>RoboFriends App</h1>
             <input type='text' onChange={props.searchChange } />
             </div>
     )
